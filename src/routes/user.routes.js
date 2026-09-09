d19c2d2f632da6c8b34b9d2b10cb27291e67cb42
Robins Kumar <robinstewart1818@yahoo.com>
2026-09-09 15:12:06 +0530

    Authentication login and logout

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,25 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser,loginUser,logoutUser } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.js"
+import { verifyJWT } from "../middlewares/authentication.middlewares.js";
 
 const router = Router();
 
-router.route('/register').post(registerUser)
+router.route('/register').post(upload.fields([
+  {
+    name: 'avatar',
+    maxCount:1
+  },
+    {
+    name: 'coverImage',
+    maxCount:1
+  }
+]),registerUser)
 
+router.route('/login').post(loginUser)
 
-
+//secured routes
+router.route('/logout').post(verifyJWT,logoutUser)
 
 
 
